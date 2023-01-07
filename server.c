4c307b34661b5277b1042aec2069231eca675967
yoavgilad04 <yoavgilad1995g@gmail.com>
2023-01-07 17:34:48 +0200

    fix

--- server.c
+++ server.c
@@ -55,8 +55,8 @@ void* thread_routine(struct routine_args* args) {
     stats.stat_thread.count_static = 0;
     stats.stat_thread.count_dyn = 0;
     stats.stat_thread.thread_id = args->i;
-    Queue q_waiting = args->q_arr[0];
-    Queue q_handled = args->q_arr[1];
+    Queue q_waiting = args->waiting;
+    Queue q_handled = args->handling;
     while(1){
         pthread_mutex_lock(&m_queues_size);
 

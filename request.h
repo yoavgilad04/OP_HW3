f16fb9163890e727c598dfaf0d28d7a000ae3d0c
yoavgilad04 <yoavgilad1995g@gmail.com>
2023-01-07 16:49:54 +0200

    fix

--- request.h
+++ request.h
@@ -3,17 +3,17 @@
 typedef struct stats Stats;
 void requestHandle(int fd, Stats stats);
 
-struct stats{
-    struct timeval arrival_time;
-    struct timeval handled_time;
-    struct stats_thread thread;
-};
-
 struct stats_thread{
     int thread_id;
     int count;
     int count_static;
     int count_dyn;
+};
+
+struct stats{
+    struct timeval arrival_time;
+    struct timeval handled_time;
+    struct stats_thread thread;
 };
 
 
